c0b84bc033e5ceb6bb287ae4b520c20ace668b1c
SoloPlayer-anwar <arabainahhj@gmail.com>
2023-04-27 00:02:07 +0700

    first commit

--- app/src/main/java/com/twincom/chatgpt/network/EndPoint.kt
+++ app/src/main/java/com/twincom/chatgpt/network/EndPoint.kt
@@ -1,21 +1,6 @@
 package com.twincom.chatgpt.network
 
 
-
-
-import com.twincom.chatgpt.requestbody.RequestBody
-import com.twincom.chatgpt.response.ChatGptResponse
-import io.reactivex.Observable
-import retrofit2.Response
-import retrofit2.http.Body
-import retrofit2.http.Header
-import retrofit2.http.POST
-
 interface EndPoint {
-    @POST("completions")
-    fun submitChatGpt(
-        @Header("Content-Type") contentType: String,
-        @Header("Authorization") authHeader: String,
-        @Body requestBody: RequestBody
-    ) : Observable<Response<ChatGptResponse>>
+
 }
